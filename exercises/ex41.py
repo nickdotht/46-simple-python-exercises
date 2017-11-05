7978d6ba3dffe7bc14166a78e70b1c16600e9198
Joe-Bernick Rameau <r4meau@gmail.com>
2017-11-05 14:00:13 -0500

    Hotfix

--- exercises/ex41.py
+++ exercises/ex41.py
@@ -25,29 +25,29 @@ from random import randrange
 def lingo(words):
     # Choose a word from the list randomly
     hidden = words[randrange(0, len(words))]
-    
-    # map to build clue
+
+    # Map to build clue
     sym_map = {0: "%s", 1:"(%s)", 2: "[%s]"}
     def lingo_finder(guess):
         # 0: letter not present in the hidden word
         # 1: letter present but position is wrong
         # 2: correct letter and position
         cenc = [(int(guess[i] == hidden[i])) + int(guess[i] in hidden) for i in range(5)] 
-	# enclose the letter with appropriate symbols using sym_map
-        print("".join([sym_map[cenc[i]]%guess[i] for i in range(5)]))
+        # Enclose the letter with appropriate symbols using sym_map
+        print "".join([sym_map[cenc[i]]%guess[i] for i in range(5)])
         return hidden != guess
-    
+        print
+
     if len(hidden) == 5:
         is_wrong=True
-	# until guess is right
+        # Until guess is right
         while is_wrong:
             # If user provides a word longer than 5 characters, we
             # truncate it
-            guess = input()[:5]
+            guess = raw_input()[:5]
             is_wrong = lingo_finder(guess.lower())
-        print("You guessed it!")
+        print "You guessed it!"
 
 
-#test
+# Test
 lingo(['alley', 'tally', 'valet', 'tiger', 'house', 'cigar', 'opera', 'modem', 'horse', 'plane', 'white', 'fiery'])
-
